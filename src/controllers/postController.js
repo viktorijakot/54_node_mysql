40987ad90bf6284b556d7c3fd2efcb5c0c205e5e
viktorija <viktorija.kotaite@gmail.com>
2024-01-15 13:40:19 +0200

    postControllers

--- src/controllers/postController.js
+++ src/controllers/postController.js
@@ -38,10 +38,133 @@ module.exports.getAll = async (req, res) => {
   // }
 };
 
-module.exports.getSingle = () => {};
-module.exports.delete = () => {};
+module.exports.getSingle = async (req, res, next) => {
+  // let connection;
+  const postId = req.params.postId;
 
-module.exports.create = () => {};
+  //ar autorizuotas?
+
+  const sql = "SELECT * FROM posts WHERE post_id=?";
+  const [postArr, error] = await getSqlData(sql, [postId]);
+  if (error) {
+    // console.log(error);
+    // res.status(500).json("something wrong");
+    return next(error);
+  }
+  if (postArr.length === 1) {
+    res.json(postArr[0]);
+    return;
+  }
+  if (postArr.length === 0) {
+    // res.status(404).json({ msg: "post not found" });
+    next({ message: "Post not found", status: 404 });
+    return;
+  }
+  res.status(400).json(postArr);
+  // try {
+  //   connection = await mysql.createConnection(dbConfig);
+  //   // const [rows, fields] = await connection.query("SELECT * FROM `posts`");
+  //   // const post = rows.find((rObj) => rObj.post_id === postId);
+  //   // ?, kad nenulauztu, kai kintamaji naudojam
+  //   const sql = "SELECT * FROM posts WHERE post_id=?";
+  //   const [rows] = await connection.execute(sql, [postId]);
+
+  //   //radom viena irasa
+  //   if (rows.length === 1) {
+  //     res.json(rows[0]);
+  //     return;
+  //   }
+  //   //radom daugiau nei viena, neradom nei vieno
+  //   res.json(rows);
+  //   // res.status(400).json(post);
+  // } catch (error) {
+  //   console.warn("/api/posts", error);
+  //   res.status(500).json("something wrong");
+  // } finally {
+  //   connection?.end();
+  // }
+};
+
+module.exports.delete = async (req, res) => {
+  // let connection;
+  const postId = req.params.postId;
+  const sql = "DELETE FROM posts WHERE post_id=? LIMIT 1";
+  const [postArr, error] = await getSqlData(sql, [postId]);
+  if (error) {
+    // console.log(error);
+    // res.status(500).json("something wrong");
+    // return;
+    return next(error);
+  }
+  if (postArr.affectedRows === 1) {
+    res.json({ msg: `post with id ${postId} was deleted` });
+    return;
+  }
+  res.status(400).json({ msg: `no rows affected`, postArr });
+
+  // try {
+  //   connection = await mysql.createConnection(dbConfig);
+  //   const sql = "DELETE FROM posts WHERE post_id=? LIMIT 1";
+  //   const [rows] = await connection.execute(sql, [postId]);
+  //   if (rows.affectedRows === 1) {
+  //     res.json({ msg: `post with id ${postId} was deleted` });
+  //     return;
+  //   }
+  //   res.status(400).json({ msg: `no rows affected`, rows });
+  // } catch (error) {
+  //   console.warn("/api/posts", error);
+  //   res.status(500).json("something wrong");
+  // } finally {
+  //   connection?.end();
+  // }
+};
+
+module.exports.create = async (req, res) => {
+  const { title, author, date, content, cat_id: catId } = req.body;
+  const sql = `
+      INSERT INTO posts (title, author, date, content, cat_id)
+      VALUES (?, ?, ?, ?, ?)
+      `;
+  const [postArr, error] = await getSqlData(sql, [
+    title,
+    author,
+    date,
+    content,
+    catId,
+  ]);
+  if (error) {
+    // console.log(error);
+    // res.status(500).json("something wrong");
+    // return;
+    return next(error);
+  }
+  res.json(postArr);
+
+  //validation
+
+  // let connection;
+  // try {
+  //   connection = await mysql.createConnection(dbConfig);
+  //   const sql = `
+  //     INSERT INTO posts (title, author, date, content, cat_id)
+  //     VALUES (?, ?, ?, ?, ?)
+  //     `;
+  //   const [rowObj] = await connection.execute(sql, [
+  //     title,
+  //     author,
+  //     date,
+  //     content,
+  //     catId,
+  //   ]);
+  //   res.json(rowObj);
+  // } catch (error) {
+  //   console.warn("/api/posts", error);
+  //   res.status(500).json("something wrong");
+  // } finally {
+  //   connection?.end();
+  // }
+  // // res.json("sukurti");
+};
 
 // module.exports = {
 //   getAll,
--- src/routes/postRoutes.js
+++ src/routes/postRoutes.js
@@ -1,7 +1,4 @@
 const express = require("express");
-const mysql = require("mysql2/promise");
-const { dbConfig, jwtSecret } = require("../config");
-const { getSqlData, getSqlDataNoTry } = require("./helper");
 const { authorizeToken, validatePostBody } = require("../middleware");
 const postController = require("../controllers/postController");
 
@@ -12,88 +9,11 @@ const postRouter = express.Router();
 postRouter.get("/api/posts", authorizeToken, postController.getAll);
 //routas tik autorizuotiem vartotojam
 
-postRouter.get("/api/posts/:postId", authorizeToken, async (req, res, next) => {
-  // let connection;
-  const postId = req.params.postId;
-
-  //ar autorizuotas?
-
-  const sql = "SELECT * FROM posts WHERE post_id=?";
-  const [postArr, error] = await getSqlData(sql, [postId]);
-  if (error) {
-    // console.log(error);
-    // res.status(500).json("something wrong");
-    return next(error);
-  }
-  if (postArr.length === 1) {
-    res.json(postArr[0]);
-    return;
-  }
-  if (postArr.length === 0) {
-    // res.status(404).json({ msg: "post not found" });
-    next({ message: "Post not found", status: 404 });
-    return;
-  }
-  res.status(400).json(postArr);
-  // try {
-  //   connection = await mysql.createConnection(dbConfig);
-  //   // const [rows, fields] = await connection.query("SELECT * FROM `posts`");
-  //   // const post = rows.find((rObj) => rObj.post_id === postId);
-  //   // ?, kad nenulauztu, kai kintamaji naudojam
-  //   const sql = "SELECT * FROM posts WHERE post_id=?";
-  //   const [rows] = await connection.execute(sql, [postId]);
-
-  //   //radom viena irasa
-  //   if (rows.length === 1) {
-  //     res.json(rows[0]);
-  //     return;
-  //   }
-  //   //radom daugiau nei viena, neradom nei vieno
-  //   res.json(rows);
-  //   // res.status(400).json(post);
-  // } catch (error) {
-  //   console.warn("/api/posts", error);
-  //   res.status(500).json("something wrong");
-  // } finally {
-  //   connection?.end();
-  // }
-});
+postRouter.get("/api/posts/:postId", authorizeToken, postController.getSingle);
 
 //delete
 
-postRouter.delete("/api/posts/:postId", authorizeToken, async (req, res) => {
-  // let connection;
-  const postId = req.params.postId;
-  const sql = "DELETE FROM posts WHERE post_id=? LIMIT 1";
-  const [postArr, error] = await getSqlData(sql, [postId]);
-  if (error) {
-    // console.log(error);
-    // res.status(500).json("something wrong");
-    // return;
-    return next(error);
-  }
-  if (postArr.affectedRows === 1) {
-    res.json({ msg: `post with id ${postId} was deleted` });
-    return;
-  }
-  res.status(400).json({ msg: `no rows affected`, postArr });
-
-  // try {
-  //   connection = await mysql.createConnection(dbConfig);
-  //   const sql = "DELETE FROM posts WHERE post_id=? LIMIT 1";
-  //   const [rows] = await connection.execute(sql, [postId]);
-  //   if (rows.affectedRows === 1) {
-  //     res.json({ msg: `post with id ${postId} was deleted` });
-  //     return;
-  //   }
-  //   res.status(400).json({ msg: `no rows affected`, rows });
-  // } catch (error) {
-  //   console.warn("/api/posts", error);
-  //   res.status(500).json("something wrong");
-  // } finally {
-  //   connection?.end();
-  // }
-});
+postRouter.delete("/api/posts/:postId", authorizeToken, postController.delete);
 
 //POST /api/posts - sukuria nauja posta
 
@@ -101,52 +21,7 @@ postRouter.post(
   "/api/posts",
   authorizeToken,
   validatePostBody,
-  async (req, res) => {
-    const { title, author, date, content, cat_id: catId } = req.body;
-    const sql = `
-      INSERT INTO posts (title, author, date, content, cat_id)
-      VALUES (?, ?, ?, ?, ?)
-      `;
-    const [postArr, error] = await getSqlData(sql, [
-      title,
-      author,
-      date,
-      content,
-      catId,
-    ]);
-    if (error) {
-      // console.log(error);
-      // res.status(500).json("something wrong");
-      // return;
-      return next(error);
-    }
-    res.json(postArr);
-
-    //validation
-
-    // let connection;
-    // try {
-    //   connection = await mysql.createConnection(dbConfig);
-    //   const sql = `
-    //     INSERT INTO posts (title, author, date, content, cat_id)
-    //     VALUES (?, ?, ?, ?, ?)
-    //     `;
-    //   const [rowObj] = await connection.execute(sql, [
-    //     title,
-    //     author,
-    //     date,
-    //     content,
-    //     catId,
-    //   ]);
-    //   res.json(rowObj);
-    // } catch (error) {
-    //   console.warn("/api/posts", error);
-    //   res.status(500).json("something wrong");
-    // } finally {
-    //   connection?.end();
-    // }
-    // // res.json("sukurti");
-  }
+  postController.create
 );
 
 module.exports = postRouter;
